9d6e629788b72a40a954cd83665b42e57dfc7e2d
munnarawat <munnamanoj620@gmail.com>
2026-01-03 17:51:29 +0530

    feat: enhance user registration with error handling and JWT token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,17 +2,57 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcryptJs = require("bcryptjs");
 
-const registerController = async (req,res)=>{
+const registerController = async (req, res) => {
+  const {
+    userName,
+    fullName: { firstName, lastName },
+    email,
+    password,
+  } = req.body;
 
-    const {fullName:{firstName,lastName},email, password} = req.body;
-
-    const userExits = await userModel.findOne({
-        email
+  const userExits = await userModel.findOne({
+    $or: [{ userName, email }],
+  });
+  if (userExits) {
+    return res.status(400).json({
+      message: "User already exits",
     });
-    
-}
+  }
 
+  const hasPassword = await bcryptJs.hash(password, 10);
 
-module.exports ={
-    registerController 
-}
+  const user = await userModel.create({
+    userName,
+    fullName: {
+      firstName,
+      lastName,
+    },
+    email,
+    password: hasPassword,
+  });
+
+  const token = await jwt.sign(
+    { id: user._id, userName: user.userName, email: user.email },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "1d" }
+  );
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000, //1 day
+  });
+
+  res.status(201).json({
+    message:"user register successfully 🎉🎉",
+    user:{
+        userName,
+        email,
+        firstName,
+        lastName
+    }
+  })
+};
+
+module.exports = {
+  registerController,
+};
