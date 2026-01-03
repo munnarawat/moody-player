a8179b6d5e7766df3bb42342d3c974a1b5d9974d
munnarawat <munnamanoj620@gmail.com>
2026-01-03 17:51:54 +0530

    fix: correct user model schema definition and update auth route initialization

--- backend/src/routers/auth.route.js
+++ backend/src/routers/auth.route.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const  authControllers = require("../controllers/auth.controller")
 
+const route = express.Router();
 
-const route = express.Router;
+// call register  api 
+
+route.post("/register",authControllers.registerController);
 
 
 
