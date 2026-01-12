ac8883593f78149c4e70115f2336cb78c5ac7959
munnarawat <munnamanoj620@gmail.com>
2026-01-12 17:33:46 +0530

    feat: implement user authentication check on app load; enhance loading state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "./router/AppRouter";
 import NavBar from "./components/nav/NavBar";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setUser } from "./store/authSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const [isCheckingAuth, setIsCheckingAuth] = useState(true);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/auth/", {
+          withCredentials: true,
+        });
+        if (response.data.user) {
+          dispatch(setUser(response.data.user));
+        }
+      } catch (error) {
+        console.log("User not logged in or session expired");
+        dispatch(setUser(null));
+      } finally {
+        setIsCheckingAuth(false);
+      }
+    };
+    checkUser();
+  }, [dispatch]);
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center bg-black text-white">
+        Loading...
+      </div>
+    );
+  }
   return (
     <div className="w-full h-full ">
-      <NavBar/>
-      <AppRouter /> 
+      <NavBar />
+      <AppRouter />
     </div>
   );
 };
