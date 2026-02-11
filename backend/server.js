fd40de34ca85760fbe7d8fe950e09d674c5fba51
munnarawat <munnamanoj620@gmail.com>
2026-02-11 16:57:57 +0530

    refactor: update server.js to use PORT environment variable for dynamic port assignment; clean up package.json formatting

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,5 @@ const ConnectToDB = require("./src/db/db");
 ConnectToDB();
 
 
-app.listen(3000 , ()=>{
-    console.log("server is running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
